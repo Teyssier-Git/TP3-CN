4f7fa76057d66c0f81dcfff9ede999859591a42b
Theo <theo26200@gmail.com>
2020-06-08 15:18:36 +0200

    Finish decodage algorithm

--- Makefile
+++ Makefile
@@ -3,12 +3,15 @@ CFLAGS = -Wall -Werror
 OBJECTS = $(patsubst %.c, %.o, $(wildcard *.c))
 RM = rm -f
 
-all: clean codageConv
+all: clean codageConv decodageConv
 
 codageConv: codageConv.c
 	$(CC) -o codageConv codageConv.c
 
+decodageConv: decodageConv.c
+	$(CC) -o decodageConv decodageConv.c
+
 .PHONY: clean
 
 clean:
-	$(RM) $(OBJECTS) codageConv
+	$(RM) $(OBJECTS) codageConv decodageConv
--- decodageConv.c
+++ decodageConv.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
-#define MESSAGE_SIZE 8
-
-//ecrire des defines pour les transitions, plus simple
-
-int time = 0
+#define BUFFER_SIZE 8
 
 struct way {
-    int message[MESSAGE_SIZE];
+    int message[BUFFER_SIZE];
     int size; // size -1 : any way which come to the state
     int weight;
 };
@@ -20,21 +17,69 @@ struct ways {
 
 struct ways chemins;
 
+struct couple {
+    int out1;
+    int out2;
+
+    int state;
+
+    int sortie;
+};
+
+struct couple trans[4][2];
+
 void init() {
     for (int i =0; i<4; i++) {
-        chemins.way[i].size = -1;
-        chemins.way[i].weight = 0;
+        chemins.way[i].size = 0;
+        chemins.way[i].weight = INT_MAX;
     }
-    chemins.way[0].size = 0;
+    chemins.way[0].weight = 0;
 }
 
-void treatment () {
-    struct ways temp = chemin;
-    for (int i=0; i<4; i++) {
-        if (chemins.way[i].size > -1) {
-            // traiter en fonction de comment c'est
+void copy(struct way *dest, struct way *src) {
+    for (int i=0; i<src->size; i++) {
+        dest->message[i] = src->message[i];
+    }
+}
 
-            // compare avec le poid chemin final
+void treatment (int b1, int b2) {
+    struct ways temp = chemins;
+    for (int i=0; i<4; i++) {
+        struct way w1, w2;
+
+
+        struct couple c = trans[i][0];
+        copy(&w1,&temp.way[c.state]);
+        int weight = 0;
+        if (b1 != c.out1) {
+            weight++;
+        }
+        if (b2 != c.out2) {
+            weight++;
+        }
+        w1.weight = weight + temp.way[c.state].weight;
+        w1.message[temp.way[c.state].size] = c.sortie;
+        w1.size = temp.way[c.state].size + 1;
+
+
+        c = trans[i][1];
+        copy(&w2,&temp.way[c.state]);
+        weight = 0;
+        if (b1 != c.out1) {
+            weight++;
+        }
+        if (b2 != c.out2) {
+            weight++;
+        }
+        w2.weight = weight + temp.way[c.state].weight;
+        w2.message[temp.way[c.state].size] = c.sortie;
+        w2.size = temp.way[c.state].size + 1;
+
+
+        if (w1.weight > w2.weight) {
+            chemins.way[i] = w2;
+        } else {
+            chemins.way[i] = w1;
         }
     }
 }
@@ -53,8 +98,58 @@ int main (int argc, char * argv[]) {
         printf("Error : Sequence only pair\n");
     }
 
-    init();
+    trans[0][0] = (struct couple){0,0,0,0};
+    trans[0][1] = (struct couple){1,1,1,0};
+    trans[1][0] = (struct couple){1,0,2,0};
+    trans[1][1] = (struct couple){0,1,3,0};
+    trans[2][0] = (struct couple){1,1,0,1};
+    trans[2][1] = (struct couple){0,0,1,1};
+    trans[3][0] = (struct couple){0,1,2,1};
+    trans[3][1] = (struct couple){1,0,3,1};
 
-    // Traiter la séquence 8 par 8
-    // Reinitialiser a la fin de 8 temps la structure goblale 
+
+    int finalDecode[size/2];
+    for (int i=0; i<size/2; i++) {
+        finalDecode[i] = 0;
+    }
+
+    int i = 0;
+    for (; i<size; i+=2) {
+        if (i%(BUFFER_SIZE*2) == 0) {
+            if (i != 0) {
+                struct way temp = chemins.way[0];
+                for (int j=1; j<4; j++) {
+                    if (chemins.way[j].weight < temp.weight) {
+                        temp = chemins.way[j];
+                    }
+                }
+
+                for (int j=0; j<BUFFER_SIZE; j++) {
+                    finalDecode[i/2-BUFFER_SIZE+j] = temp.message[j];
+                }
+            }
+            init();
+        }
+
+        treatment((int)(sequence[i]-'0'), (int)(sequence[i+1]-'0'));
+    }
+
+    struct way temp = chemins.way[0];
+    for (int j=1; j<4; j++) {
+        if (chemins.way[j].weight < temp.weight) {
+            temp = chemins.way[j];
+        }
+    }
+
+    for (int j=0; j<temp.size; j++) {
+        finalDecode[i/2-8+j] = temp.message[j];
+    }
+
+
+
+    printf("Message : ");
+    for (int i = 0; i<size/2; i++) {
+        printf("%d",finalDecode[i]);
+    }
+    printf("\n");
 }
